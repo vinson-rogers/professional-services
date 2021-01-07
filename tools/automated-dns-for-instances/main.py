e48474c2e28ad0db4a4855c4d512898ba63e04bd
Rogers, Vinson <Vinson.Rogers.ctr@sabre.com>
2021-01-06 19:16:30 -0600

    bug fixes, added custom dns via label

--- tools/automated-dns-for-instances/main.py
+++ tools/automated-dns-for-instances/main.py
@@ -16,10 +16,13 @@ from google.cloud import dns
 import time
 import re
 
-PROJECT_ID=<PROJECT_ID>
-ZONE=<ZONE_ID>
-DOMAIN=<DOMAIN> # should have a trailing dot
+PROJECT_ID='storied-glazing-255921'
+ZONE='test-zone'
+DOMAIN='sabre-gcp.com.'
 TTL=3600
+
+env_folders = { '659459957450': 'dev', '414067823882': 'prod' }
+bu_folders = { '1021170792996': 'shs', '1085585681071': 'shs' }
 
 client = dns.Client(project=PROJECT_ID)
 zone = client.zone(ZONE, DOMAIN)
@@ -28,34 +31,63 @@ def find_by_name(name):
     records = zone.list_resource_record_sets()
     for record in records:
         if name in record.name:
+            print('Found existing record:',record.name)
             rs = zone.resource_record_set(record.name, record.record_type, record.ttl, record.rrdatas)
             return(rs)
     return False
+
+def get_env_and_bu(data):
+    bu = 'default'
+    env = 'default'
+    # determine env/bu folder then map to portions of dns name
+    for i in data['asset']['ancestors']:
+        #print(i)
+        match = re.search(r"folders\/(.+)", i)
+        if match is not None and match[1] in env_folders:
+            #print(match[1])
+            #print(env_folders[match[1]])
+            env = env_folders[match[1]]
+        if match is not None and match[1] in bu_folders:
+            #print(match[1])
+            #print(bu_folders[match[1]])
+            bu = bu_folders[match[1]]
+    return(bu,env)
 
 def vmToDNS(event, context):
     import base64, json
     changes=zone.changes()
     valid_statuses = ['STAGING','DELETED']
+    bu = 'default'
+    env = 'default'
 
     if 'data' in event:
         data = base64.b64decode(event['data']).decode('utf-8')
         data = json.loads(data)
-        # print(data)
+        print(data)
         if 'deleted' in data and data['deleted'] == True:
             status = 'DELETED'
-            # print(data['asset']['name'])
+            print('Will DELETE',data['asset']['name'])
             match = re.search(r"/instances\/(.+)", data['asset']['name'])
-            # print(match[1])
-            if match:
+            if match is not None:
+                print(match[1])
                 name = match[1]
+            if 'labels' in data['priorAsset']['resource']['data']:
+                if 'dns-name' in data['priorAsset']['resource']['data']['labels']:
+                    name = data['priorAsset']['resource']['data']['labels']['dns-name']
         else:
             status = data['asset']['resource']['data']['status']
+
+        bu,env = get_env_and_bu(data)
 
     # set vars for valid statuses
     if 'STAGING' in status:
         print(f'Handling status', status)
         name = data['asset']['resource']['data']['name']
+        if 'labels' in data['asset']['resource']['data']:
+            if 'dns-name' in data['asset']['resource']['data']['labels']:
+                name = data['asset']['resource']['data']['labels']['dns-name']
         ip = data['asset']['resource']['data']['networkInterfaces'][0]['networkIP']
+
     elif 'DELETED' in status:
         pass
     else:
@@ -63,10 +95,14 @@ def vmToDNS(event, context):
         return(True)
 
     # check for existing and mark for deletion
+    name = f'{name}.{bu}.{env}'
     del_record_set = find_by_name(name)
     if del_record_set is not False:
         print('Deleting existing record for', name)
         changes.delete_record_set(del_record_set)
+    # clear status for deleted if we find no records
+    elif 'deleted' in data and data['deleted'] == True:
+        status = 'noop'
 
     # add records for new VMs
     if 'STAGING' in status and name != '' and ip != '':
@@ -78,8 +114,8 @@ def vmToDNS(event, context):
     # execute changes for valid statuses
     if any(x in status for x in valid_statuses):
         changes.create()
-        #print(changes.status)
+        print(changes.status)
         while changes.status != 'done':
             time.sleep(.1)
             changes.reload()
-        #print('Changes status:', changes.status)
+        print('Changes status:', changes.status)
